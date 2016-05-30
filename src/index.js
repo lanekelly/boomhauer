ae9f9074a26e86b37bd3254d95129b81ca9444c9
Lane Kelly <lanekelly16@gmail.com>
2016-05-30 11:24:53 -0700

    added eslint, made episodes into json file

--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@
 
 'use strict';
 
-var AlexaSkill = require('./AlexaSkill');
-var Episodes = require('./episodes');
+const AlexaSkill = require('./AlexaSkill');
+const Episodes = require('./episodes');
 
-var Boomhauer = function () {
+const Boomhauer = function Boomhauer() {
   AlexaSkill.call(this); // add APP_ID here later
 };
 
 Boomhauer.prototype = Object.create(AlexaSkill.prototype);
 Boomhauer.prototype.constructor = Boomhauer;
 
-Boomhauer.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-  var speechText = "Ask me for an episode of King of the Hill. You can even specify a season.";
-  var repromptText = "For example, ask me to give you an episode from season one.";
+Boomhauer.prototype.eventHandlers.onLaunch = function onLaunch(launchRequest, session, response) {
+  const speechText = 'Ask me for an episode of King of the Hill. You can even specify a season.';
+  const repromptText = 'For example, ask me to give you an episode from season one.';
 
   response.ask(speechText, repromptText);
 };
 
 Boomhauer.prototype.intentHandlers = {
-  "EpisodeIntent": function (intent, session, response) {
-    var seasonSlot = intent.slots.Season;
-    var seasonNumber;
+  EpisodeIntent(intent, session, response) {
+    const seasonSlot = intent.slots.Season;
 
+    let seasonNumber;
     if (seasonSlot && seasonSlot.value) {
       // check if converting is really necessary
       // might need to convert to a Number instead
@@ -37,27 +37,27 @@ Boomhauer.prototype.intentHandlers = {
       return;
     }
 
-    var resp = "";
+    let resp = '';
 
-    var season = Episodes[seasonNumber];
+    const season = Episodes[seasonNumber];
     if (season) {
       // for now, just get first in list
-      var episode = season[0];
-      resp = "In episode " + episode.number + ", titled " + episode.title + ", " + episode.description;
+      const episode = season[0];
+      resp = `In episode ${episode.number}, titled ${episode.title}, ${episode.description}`;
     } else {
-      resp = "No data for season " + seasonNumber;
+      resp = `No data for season ${seasonNumber}`;
     }
 
-    var speechOutput = {
+    const speechOutput = {
       speech: resp,
       type: AlexaSkill.speechOutputType.PLAIN_TEXT
     };
 
-    response.tellWithCard(speechOutput, "King of the Hill", resp);
+    response.tellWithCard(speechOutput, 'King of the Hill', resp);
   }
 };
 
-exports.handler = function (event, context) {
-  var boomhauer = new Boomhauer();
+exports.handler = function handler(event, context) {
+  const boomhauer = new Boomhauer();
   boomhauer.execute(event, context);
 };
